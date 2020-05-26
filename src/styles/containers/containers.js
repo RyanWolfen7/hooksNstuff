e09f64d6ce8d747984697cb567b47bd8fc4f4b08
RyanWolfen7 <wolfen7lada@gmail.com>
2020-05-26 15:43:17 -0500

    added ability to change grid size

--- src/styles/containers/containers.js
+++ src/styles/containers/containers.js
@@ -3,8 +3,8 @@ import { gridRepeat } from '../../helpers/styles'
 
 export const AppContainer = styled.div`
     display: grid;
-    grid-template-columns: ${props => { return gridRepeat(5, props.col) }};
-    grid-template-rows: ${props => { return gridRepeat(10, props.row)}};
+    grid-template-columns: ${props => { return gridRepeat(5, props.col, props.colSize) }};
+    grid-template-rows: ${props => { return gridRepeat(10, props.row, props.rowSize)}};
     align-content: stretch;
     margin: 0 5vw 0;
     height: 100vh;
